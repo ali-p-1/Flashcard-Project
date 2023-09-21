0d0a078081184d796249e757275c5413cd75f293
ali-p-1 <alipirzada06@gmail.com>
2023-09-22 00:01:49 +0100

    Login system with Database

--- FlashCards.py
+++ FlashCards.py
@@ -1,18 +1,27 @@
 import PyQt5 as py
 import sys
+import sqlite3
 from PyQt5 import QtWidgets, uic
 from PyQt5.QtWidgets import *
+from DB import db_creation
+
+db_creation()
 
 class GUI(QMainWindow):
     def __init__(self):
         super(GUI, self).__init__()
         uic.loadUi("./HomePage.ui", self)
         self.show()
-
         self.loginb.clicked.connect(self.login)
 
-    def login(self):
-        if self.username.text() == "test1" and self.password.text() == "pass1":
+    def login(self): 
+        user = self.username.text()
+        passw = self.password.text()
+        connection = sqlite3.connect("Flashcard_Project.db")
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM users WHERE Username = ? AND Password = ? ", (user, passw))
+        result = cursor.fetchone()
+        if result:
             self.success.setEnabled(True)
             message2 = QMessageBox()
             message2.setText("Login Successful")
@@ -21,6 +30,7 @@ class GUI(QMainWindow):
             message = QMessageBox()
             message.setText("Invalid Login")
             message.exec()
+
 
 def main():
     app = QApplication([])
