bba40829a8d668c11a0b019dadef6715893436e3
ali-p-1 <alipirzada06@gmail.com>
2023-10-03 22:30:51 +0100

    Database tables created

--- DB.py
+++ DB.py
@@ -1,8 +1,9 @@
 from PyQt5 import QtSql
 import sqlite3
+db_path=("C:/Users/Ali Pirzada/Documents/Projects/CourseWork/Flashcard_Project.db")
 
 def db_creation():
-    db_path=("C:/Users/Ali Pirzada/Documents/Projects/CourseWork/Flashcard_Project.db")
+    global db_path
     admin = "admin"
     adminpass = "pass"
     connection = sqlite3.connect(db_path)
@@ -20,4 +21,61 @@ def db_creation():
     cursor.close()
     connection.close()
 
+def folder_table():
+    global db_path
+    connection = sqlite3.connect(db_path)
+    cursor = connection.cursor()
+    create_folders_table_query = """
+    CREATE TABLE IF NOT EXISTS Folders (
+        FolderID INTEGER PRIMARY KEY,
+        user_id INTEGER,
+        fname TEXT,
+        FOREIGN KEY (user_id) REFERENCES users(id)
+    )
+    """
+    cursor.execute(create_folders_table_query)
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+
+def sets_table():
+    global db_path
+    connection = sqlite3.connect(db_path)
+    cursor = connection.cursor()
+    create_sets_table_query = """
+    CREATE TABLE IF NOT EXISTS Sets (
+        SetID INTEGER PRIMARY KEY,
+        FolderID INTEGER,
+        set_name TEXT,
+        FOREIGN KEY (FolderID) REFERENCES Folders(FolderID) 
+    )
+    """
+    cursor.execute(create_sets_table_query)
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+def flashcard():
+    global db_path
+    connection = sqlite3.connect(db_path)
+    cursor = connection.cursor()
+    create_flashcards_table_query = """
+    CREATE TABLE IF NOT EXISTS Flashcards (
+        flashcard_ID INTEGER PRIMARY KEY,
+        set_id INTEGER,
+        front_text TEXT,
+        back_text TEXT,
+        FOREIGN KEY(set_id) REFERENCES Sets(SetID)
+        )
+        """
+    cursor.execute(create_flashcards_table_query)
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+
 db_creation()
+folder_table()
+sets_table()
+flashcard()
