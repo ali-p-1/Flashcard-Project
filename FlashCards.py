24edc4217fa8b4db8fda56103a3aba62c4e457a6
ali-p-1 <alipirzada06@gmail.com>
2023-11-21 22:59:22 +0000

    Login system with UI and Create new user

--- FlashCards.py
+++ FlashCards.py
@@ -3,33 +3,55 @@ import sys
 import sqlite3
 from PyQt5 import QtWidgets, uic
 from PyQt5.QtWidgets import *
-from DB import db_creation
+# from DB import create_database
 
-db_creation()
-
+# create_database() 
+ 
 class GUI(QMainWindow):
     def __init__(self):
-        super(GUI, self).__init__()
-        uic.loadUi("./HomePage.ui", self)
+        super().__init__()
+        uic.loadUi(r"C:\Users\Ali Pirzada\Documents\Projects\CourseWork\gui.ui" , self)
         self.show()
         self.loginb.clicked.connect(self.login)
+        self.createuser.clicked.connect(self.newuser)
 
-    def login(self): 
-        user = self.username.text()
-        passw = self.password.text()
+
+    def login(self):
+        user = self.uname.text()
+        passw = self.passw.text()
         connection = sqlite3.connect("Flashcard_Project.db")
         cursor = connection.cursor()
-        cursor.execute("SELECT * FROM users WHERE Username = ? AND Password = ? ", (user, passw))
+        cursor.execute("SELECT * FROM Users WHERE username = ? AND password = ?" , (user, passw))
         result = cursor.fetchone()
-        if result:
-            self.success.setEnabled(True)
+        if result is not None : 
             message2 = QMessageBox()
             message2.setText("Login Successful")
             message2.exec()
         else:
             message = QMessageBox()
-            message.setText("Invalid Login")
+            message.setText("Login failed, please try again")
             message.exec()
+
+    def newuser(self):
+        new_user = self.uname.text()
+        new_passw = self.passw.text()
+        connection = sqlite3.connect("Flashcard_Project.db")
+        cursor = connection.cursor()
+        cursor.execute("INSERT INTO Users (username, password) VALUES (?, ?)" , (new_user, new_passw))
+        connection.commit()
+        message = QMessageBox()
+        message.setText("Registration successful")
+        message.exec()
+
+
+
+
+
+
+
+
+
+
 
 
 def main():
@@ -38,3 +60,4 @@ def main():
     app.exec_()
 
 main()
+
